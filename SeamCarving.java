9506acfea4069ba3b8f699efdd92f08c2d36b4c6
Frederik Stensaeth <stensaethf@carleton.edu>
2015-10-27 11:15:16 -0500

    SeamCarving method headers

--- SeamCarving.java
+++ SeamCarving.java
@@ -16,9 +16,52 @@ public class SeamCarver {
 
 	methods:
 		findSeam (finds the min seam)
-		carve (carves out the min seam)
-		print (opens the new image)
+		carve (carves out the seams)
+		removeSeam (removes a seam)
+		printCarved (opens the new image)
 		getNum/setNum (gets and sets the seam number ~ for updating purposes)
+
+	public SeamCarver(File filename, int seamNumber) {
+		this.seamNumber = seamNumber;
+		this.filename = filename;
+
+		this.carve()
+	}
+
+	private int getNum() {
+		return this.seamNumber;
+	}
+
+	private void setNum(int newSeamNumber) {
+		this.seamNumber = newSeamNumber
+	}
+
+	private List findSeam(xx) {
+		// Code
+	}
+
+	private List computeEnergy(xx) {
+		// Code
+	}
+
+	private void removeSeam(xx) {
+		// Code
+	}
+
+	private void carve(xx) {
+		// Code
+
+		while this.getNum() > 0:
+			// Code
+			// removeSeam() --> findSeam() --> computeEnergy()
+			// 				--> setNum() = getNum() - 1
+
+		// printCarved()
+	}
+
+	private void printCarved(xx) {
+		// Code
+	}
 }
 
 public static void main(String[] args) throws Exception {
